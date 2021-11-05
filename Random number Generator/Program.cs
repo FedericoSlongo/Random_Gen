189166057817b7649171e1c1b4a060f0a720e135
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2021-11-05 16:49:36 +0100

    Update Program.cs

--- Random number Generator/Program.cs
+++ Random number Generator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,15 +11,48 @@ namespace Random_number_Generator
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            int first = rnd.Next(), second = rnd.Next();
-            if (first > second)
+            Console.WriteLine("Would you like for the program to be completely at random or for you to set a range \n[r]andom, [s]et");
+
+            char user_choice = Convert.ToChar(Console.ReadLine());
+
+            switch (user_choice)
             {
-                int temp;
-                temp = first;
-                first = second;
-                second = temp;
+                case 's':
+                case 'S':
+                    int number_1, number_2;
+                    Console.WriteLine("Insert the first number ");
+                    number_1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Insert the second number ");
+                    number_2 = Convert.ToInt32(Console.ReadLine());
+
+                    if (number_1 > number_2)
+                    {
+                        int temp;
+                        temp = number_1;
+                        number_1 = number_2;
+                        number_2 = temp;
+                    }
+                    Console.WriteLine($"The generated number is {rnd.Next(number_1, number_2)}");
+                    break;
+
+                case 'r':
+                case 'R':
+                    int first = rnd.Next(), second = rnd.Next();
+                    if (first > second)
+                    {
+                        int temp;
+                        temp = first;
+                        first = second;
+                        second = temp;
+                    }
+                    Console.WriteLine($"The generated number is {rnd.Next(first, second)}");
+                    break;
+
+                default:
+                    Console.WriteLine("It's not a valid type");
+                    break;
             }
-            Console.WriteLine($"The generated number is {rnd.Next(first, second)}");
+            
             Console.ReadKey();
         }
     }
