78d719bf31fdcd49c78c4516fd41f95e1b2a60b8
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2021-11-05 15:36:59 +0100

    Add files via upload

---
+++ Random number Generator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_number_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int first = rnd.Next(), second = rnd.Next();
+            if (first > second)
+            {
+                int temp;
+                temp = first;
+                first = second;
+                second = temp;
+            }
+            Console.WriteLine($"The generated number is {rnd.Next(first, second)}");
+            Console.ReadKey();
+        }
+    }
+}
