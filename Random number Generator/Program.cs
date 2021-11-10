7502d23131e184ac8182fac3b239e0f2106af211
CoachYT1 <69034647+CoachYT1@users.noreply.github.com>
2021-11-10 09:05:05 +0100

    Reformat Code, Optimize performaces
     - Optimize Imports
     - Code is now more readable
     - RAM usage optimization
     - Swap variables with (a, b) = (b, a) statement
     - Make 'Program' static

--- Random number Generator/Program.cs
+++ Random number Generator/Program.cs
@@ -1,77 +1,48 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Random_number_Generator
+namespace RND
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Random rnd = new Random();
-            bool try_pap = false;
-            char user_choice;
+            char userChoice;
             do
             {
-                if (try_pap)
-                {
-                    Console.WriteLine("This isn't a valid type");
-                }
-                Console.WriteLine("Would you like for the program to be completely at random or for you to set a range \n[r]andom, [s]et");
-            } while (try_pap = !char.TryParse(Console.ReadLine(), out user_choice));
+                Console.Clear();
+                Console.WriteLine("Would you like for the program to be completely random or for you to set a range \n[r]andom, [s]et");
+                
+            } while (!char.TryParse(Console.ReadLine(), out userChoice) || userChoice != 's' || userChoice != 'S'|| userChoice != 'r' || userChoice != 'R');
 
-            switch (user_choice)
+            switch (userChoice)
             {
                 case 's':
                 case 'S':
-                    int number_1, number_2;
+                    int number1, number2;
                     
                     do
                     {
-                        if (try_pap)
-                        {
-                            Console.WriteLine("The number inserted isn't valid");
-                        }
+                        Console.Clear();
                         Console.WriteLine("Insert the first number ");
-                    } while (try_pap = !int.TryParse(Console.ReadLine(), out number_1));
+                    } while (!int.TryParse(Console.ReadLine(), out number1));
 
                     do
                     {
-                        if (try_pap)
-                        {
-                            Console.WriteLine("The number inserted isn't valid");
-                        }
+                        Console.Clear();
                         Console.WriteLine("Insert the second number ");
-                    } while (try_pap = !int.TryParse(Console.ReadLine(), out number_2));
+                    } while (!int.TryParse(Console.ReadLine(), out number2));
 
-                    if (number_1 > number_2)
+                    if (number1 > number2)
                     {
-                        int temp;
-                        temp = number_1;
-                        number_1 = number_2;
-                        number_2 = temp;
+                        (number1, number2) = (number2, number1);
                     }
-                    number_2++;
-                    Console.WriteLine($"The generated number is {rnd.Next(number_1, number_2)}");
+                    number2++;
+                    Console.WriteLine($"The generated number is {rnd.Next(number1, number2)}");
                     break;
-
                 case 'r':
                 case 'R':
-                    int first = rnd.Next(), second = rnd.Next();
-                    if (first > second)
-                    {
-                        int temp;
-                        temp = first;
-                        first = second;
-                        second = temp;
-                    }
-                    Console.WriteLine($"The generated number is {rnd.Next(first, second)}");
-                    break;
-
-                default:
-                    Console.WriteLine("It's not a valid type");
+                    Console.WriteLine($"The generated number is {rnd.Next()}");
                     break;
             }
             
